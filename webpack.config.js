eb7cb4edd9fb01c6f2116eb6d01d6b8ed1f819ae
Marek Bełz <marazm.lab@gmail.com>
2025-06-10 14:49:23 +0000

    initial commit with basic configuration

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js', // Punkt wejścia aplikacji
+  output: {
+    path: path.resolve(__dirname, 'dist'), // Katalog wyjściowy
+    filename: 'bundle.js', // Nazwa pliku wyjściowego
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/, // Obsługa plików JS i JSX
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader', // Użycie Babel do transpilacji
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'], // Rozpoznawane rozszerzenia
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: './public/index.html',
+        filename: 'index.html',
+    }),
+  ],
+  devServer: {
+    static: './public', // Serwowanie plików z katalogu public
+    port: 3000, // Port dev servera
+    open: true,
+  },
+  mode: 'development', // Tryb development (możesz zmienić na 'production' przy buildzie)
+};
