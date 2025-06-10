70391ba6918bed91bc825e639423591acf866607
Marek Bełz <marazm.lab@gmail.com>
2025-06-10 15:34:03 +0000

    pages added, simple nav added,

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+import Home from './pages/Home';
+import Portfolio from './pages/Portfolio';
+import Project from './pages/Project';
+import AboutMe from './pages/AboutMe';
+import Contact from './pages/Contact';
 
 const App = () => {
+
     return (
-        <div>
-            <h1>Witaj w mojej aplikacji</h1>
-        </div>
+        <Router>
+            <nav>
+                <ul>
+                    <li><Link to='/'>Home</Link></li>
+                    <li><Link to='/portfolio'>Portfolio</Link></li>
+                    <li><Link to='/about'>About Me</Link></li>
+                    <li><Link to='/contact'>Contact</Link></li>
+                </ul>
+            </nav>
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/portfolio' element={<Portfolio />} />
+                <Route path='/project/:id' element={<Project />} />
+                <Route path='/about' element={<AboutMe />} />
+                <Route path='/contact' element={<Contact />} />
+            </Routes>
+        </Router>
     );
 };
 
---
+++ src/pages/Portfolio.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Portfolio = () => {
+    return <h2>Portfolio</h2>
+}
+
+export default Portfolio;
