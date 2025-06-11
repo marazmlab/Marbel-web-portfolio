84328912f6bc3fa07fc81d8d385593740a8d25ce
Marek Bełz <marazm.lab@gmail.com>
2025-06-11 12:38:58 +0000

    basic styles added

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
+import './styles/reset.css';
+import './styles/global.css';
+
 const root = createRoot(document.getElementById('root'));
 root.render(<App />);
